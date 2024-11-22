a95671a316c14c72e46e614ae287eeb5c565c1c6
Rafael Rocha <1201260@isep.ipp.pt>
2024-11-22 12:33:14 +0000

    tiny fix.

--- Application/OperationRequest/OperationRequestDto.cs
+++ Application/OperationRequest/OperationRequestDto.cs
@@ -7,7 +7,7 @@
         public string Priority { get; set; }
         public string RecordNumber { get; set; }
         public string StaffId { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public string OperationTypeName { get; set; }
 
         public OperationRequestDto(Guid? requestId, DateTime deadLine, string priority, string recordNumber, string staffId, string status, string operationTypeName)
